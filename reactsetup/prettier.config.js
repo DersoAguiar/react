bc0da369477346a6f7bd0dadbdfbeb36eff18dc8
Anderson S Aguiar <andersonaguiar.m.z@gmail.com>
2021-09-23 14:10:51 -0300

    react

---
+++ reactsetup/prettier.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  semi: false, // PONTO E VIRGULA NO FINAL DAS LINHAS
+  singleQuote: true, // ASPAS SIMPLES
+  arrowParens: 'avoid', 
+  trailingComma: 'none', 
+  endOfLine: 'auto'
+}
